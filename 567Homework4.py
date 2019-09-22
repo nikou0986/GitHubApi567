383f919ee839fe36b952033c16d250486d762ad9
Nicole_Zhou <47457839+nikou0986@users.noreply.github.com>
2019-09-22 16:57:55 -0400

    Add files via upload
    hw4a

---
+++ 567Homework4.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class get_api():
+    repo_dict={}
+    def get_info_api(name):
+        url = 'https://api.github.com/users/'+name+'/repos'
+        r=requests.get(url)
+        print(r.status_code)
+
+
+        print("The user has "+str(len(r.json())) + " repos, names and commits as follows: ")
+        for item in r.json():
+            url_comm = 'https://api.github.com/repos/'+name+'/'+item['name']+'/commits'
+            s=requests.get(url_comm)
+            commits_list=s.json()
+            commits_dict=commits_list[0]
+            print("Name: " +item['name']+"; Commits: "+str(len(commits_dict)))
+
+        return "Right"
