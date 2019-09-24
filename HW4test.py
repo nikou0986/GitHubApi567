f95e5dc23ed8c34dce07a62e83fedfe86f3f0897
Nicole_Zhou <47457839+nikou0986@users.noreply.github.com>
2019-09-23 22:01:02 -0400

    Add files via upload
    test file

---
+++ HW4test.py
@@ -0,0 +1,21 @@
+import unittest
+from homework04 import get_api
+from nose.tools import assert_true
+import requests
+
+
+class TestApi(unittest.TestCase):
+    def testApi(self):
+        self.assertEqual(get_api.get_info_api('yalilu'), 'Right')
+        self.assertEqual(get_api.get_info_api('mhassany'), 'Right')
+
+
+#    def test_request_response(self):
+#        # Send a request to the API server and store the response.
+#        response = requests.get('http://jsonplaceholder.typicode.com/todos')
+#
+#        # Confirm that the request-response cycle completed successfully.
+#        assert_true(response.ok)
+
+if __name__ == '__main__':
+    print('Running unit tests')
